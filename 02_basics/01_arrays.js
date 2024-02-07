dd224ff73e019c5a1a4e9ec1d5a181b92a2d9d59
rudra-coder-mc <forcoding255@gmail.com>
2024-02-07 19:16:35 +0530

    day 2 , 06/02/2024
    using video of chai aur code .
    video 14-18

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7];
+
+const myArr2 = ["a", "b", "c", "d", "e"];
+
+// console.log(myArr);
+
+// console.log(myArr2);
+
+// array method
+
+// myArr.push(8);
+// myArr.pop();
+// myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const myArr2 = myArr.join()
+
+// console.log(myArr);
+// console.log( myArr2)
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3);
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1, 3);
+console.log("C ", myArr);
+console.log(myn2);
+
+
