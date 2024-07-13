502695b77fe430eec8ab14e0e9eb707576727ee0
atul modi <forcoding255@gmail.com>
2024-07-13 10:31:49 +0530

    create 30 day of javascript and complite 1 day

---
+++ 30-days-of-Javascript-challenge/1 Day/variable-and-data-types.js
@@ -0,0 +1,37 @@
+// activity 1 - variable declaration
+
+var a = 10;
+console.log(a);
+
+let b = "my name is ram";
+console.log(b);
+
+// activity 2 - constant declaration
+
+const c = true;
+console.log(c);
+
+// activity 3 - data type
+
+let d = 5; //number
+console.log(typeof d);
+let e = "name"; // string
+console.log(typeof e);
+let f = false; //boolean
+console.log(typeof f);
+let g = { id: 1, name: "name" }; // object
+console.log(typeof g);
+let h = [1, 2, 3, 4, 5]; //array
+console.log(typeof h);
+
+// activity 4 - reassiging variable
+
+let j = 10;
+console.log(j);
+j = 15;
+console.log(j);
+
+// activity 5 - understanding const
+
+const k = 10;
+// k = 11;  error 
