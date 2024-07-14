e122a19bf3f74790cec80c93ff3e43490b7b4fb2
atul modi <forcoding255@gmail.com>
2024-07-14 17:15:20 +0530

    complite 2 day

---
+++ 30-days-of-Javascript-challenge/2 day/Operation.js
@@ -0,0 +1,115 @@
+// activity 1 : arithmetic operations
+
+// const add = (a, b) => {
+//   console.log(`add two number ${a} and ${b} :`, a + b);
+// };
+
+// add(2, 3);
+// const sub = (a, b) => {
+//   console.log(`subtract two number ${a} and ${b} :`, a - b);
+// };
+
+// sub(3, 2);
+
+// const mul = (a, b) => {
+//   console.log(`multiply two number ${a} and ${b} :`, a * b);
+// };
+
+// mul(3, 2);
+
+// const div = (a, b) => {
+//   console.log(`divide two number ${a} and ${b} :`, a / b);
+// };
+
+// div(3, 2);
+
+// const rem = (a, b) => {
+//   console.log(`find the remainder of  two number ${a} and ${b} :`, a % b);
+// };
+
+// rem(3, 2);
+
+// activity 2 Assignment operators
+
+// let a = 10;
+// let b = 20;
+// let c = (a += b);
+// let d = (a -= b);
+// console.log(c, d);
+
+// activity 3 comparison operators
+
+// const comparison = (a, b) => {
+//   if (a < b) {
+//     console.log(`${a} is < then ${b}`);
+//   }
+//   if (a > b) {
+//     console.log(`${a} is > then ${b}`);
+//   }
+//   if (a >= b) {
+//     console.log(`${a} >= ${b}`);
+//   }
+//   if (a <= b) {
+//     console.log(`${a} <= ${b}`);
+//   }
+//   if (a == b) {
+//     console.log(`${a} == ${b}`);
+//   }
+//   if (a === b) {
+//     console.log(`${a} === ${b}`);
+
+//   }
+// };
+
+// comparison(3, 3);
+
+// activity 4 Logical operators
+
+// to easy don't won't to do it.
+
+// Feature Request:
+
+// ** 1 Arithmetic oprations script
+
+// const ArithmeticOpration = (a, b) => {
+//   console.log(`add two number ${a} and ${b} :`, a + b);
+//   console.log(`subtract two number ${a} and ${b} :`, a - b);
+//   console.log(`multiply two number ${a} and ${b} :`, a * b);
+//   console.log(`divide two number ${a} and ${b} :`, a / b);
+//   console.log(`find the remainder of  two number ${a} and ${b} :`, a % b);
+// };
+
+// ArithmeticOpration(2, 4);
+
+// ** 2 comparison and logical operators script
+
+// const comparison = (a, b) => {
+//   if (a < b) {
+//     console.log(`${a} is < then ${b}`);
+//   }
+//   if (a > b) {
+//     console.log(`${a} is > then ${b}`);
+//   }
+//   if (a >= b) {
+//     console.log(`${a} >= ${b}`);
+//   }
+//   if (a <= b) {
+//     console.log(`${a} <= ${b}`);
+//   }
+//   if (a == b) {
+//     console.log(`${a} == ${b}`);
+//   }
+//   if (a === b) {
+//     console.log(`${a} === ${b}`);
+//   }
+// };
+
+// comparison(0, 0);
+
+// ** 3 Ternary operator script :
+
+// const numberType = (a) => {
+//   a < 0 ? console.log(`nagivity`) : console.log(`positive`);
+// };
+
+// numberType(10)
