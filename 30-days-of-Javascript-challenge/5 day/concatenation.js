a75d4726a603880aaa7f9507a040922f053bde78
atul modi <forcoding255@gmail.com>
2024-07-20 12:41:56 +0530

    complite 5 day

---
+++ 30-days-of-Javascript-challenge/5 day/concatenation.js
@@ -0,0 +1,5 @@
+function concat(str1, str2) {
+  return str1 + " " + str2;
+}
+
+console.log(concat("atul", "modi"));
---
+++ 30-days-of-Javascript-challenge/5 day/evean-odd-fun.js
@@ -0,0 +1,10 @@
+function evanAndOdd(num) {
+  if (num % 2) {
+    console.log(num % 2);
+    return "odd";
+  } else {
+    console.log(num % 2);
+    return "even";
+  }
+}
+console.log(evanAndOdd(4));
